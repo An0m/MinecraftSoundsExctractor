6d68a62976d460a21728a5cb954c9ae0e29ce870
An0m <68523000+An0m@users.noreply.github.com>
2022-07-18 00:41:00 +0200

    Added defaults (1.19)

--- main.py
+++ main.py
@@ -3,22 +3,29 @@ import os
 import shutil
 
 assetsFolder = os.getenv("APPDATA").replace("\\", "/") + "/.minecraft/assets"
+version = input("Enter the Minecraft version (ex: 1.19) or write \"latest\" to use the latest installed: ")
 
-# Get the Minecraft version
-versions = list(filter(lambda v: str(v).startswith("1"), os.listdir(assetsFolder+"/indexes/")))
-versions = sorted([int(v[2:][:2].replace(".", "").replace("-", "")) for v in versions])
-version = f"1.{str(versions[-1])}.json"
-del versions
+# Get the latest Minecraft version
+if (version.lower() in ["latest", "auto", ""]):
+    versions = list(filter(lambda v: str(v).startswith("1"), os.listdir(assetsFolder+"/indexes/")))
+    versions = sorted([int(v[2:][:2].replace(".", "").replace("-", "")) for v in versions])
+    version = f"1.{str(versions[-1])}"
+    del versions
 
-with open(f"{assetsFolder}/indexes/{version}", "r") as f:
-    sounds = {
-        key[17:] : value["hash"] for (key, value) in json.load(f)["objects"].items() 
-        if str(key).startswith("minecraft/sounds/")}.items()
+try:
+    with open(f"{assetsFolder}/indexes/{version}.json", "r") as f:
+        sounds = {
+            key[17:] : value["hash"] for (key, value) in json.load(f)["objects"].items() 
+            if str(key).startswith("minecraft/sounds/")}.items()
 
-    # Exctract the files
-    for filePath, fileName in sounds:
-        src_fpath = f"{assetsFolder}/objects/{fileName[:2]}/{fileName}"
-        dest_fpath = f"sounds/{filePath}"
+        # Exctract the files
+        print("Extracting...")
+        for filePath, fileName in sounds:
+            src_fpath = f"{assetsFolder}/objects/{fileName[:2]}/{fileName}"
+            dest_fpath = f"{version}/{filePath}"
 
-        os.makedirs(os.path.dirname(dest_fpath), exist_ok=True)
-        shutil.copyfile(src_fpath, dest_fpath)
+            os.makedirs(os.path.dirname(dest_fpath), exist_ok=True)
+            shutil.copyfile(src_fpath, dest_fpath)
+        print("Done!")
+except:
+    print("Invalid Minecraft version")
